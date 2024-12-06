34003248df13b45a441c592ea3f8299901c098e7
Wellington Ferreira da Silva <wellingtonfs50@gmail.com>
2024-12-06 17:03:02 -0300

    Desafio concluido

--- services/api.js
+++ services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
+    baseURL: 'https://ysozsbkhqhbylxmuxhdt.supabase.co/rest/v1',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlzb3pzYmtocWhieWx4bXV4aGR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM1MTA0OTgsImV4cCI6MjA0OTA4NjQ5OH0.zfwgQYU5joT0nsaNIWsV-D_HxnKQ0Eqrmh0aXf0Fm-g",
+        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlzb3pzYmtocWhieWx4bXV4aGR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM1MTA0OTgsImV4cCI6MjA0OTA4NjQ5OH0.zfwgQYU5joT0nsaNIWsV-D_HxnKQ0Eqrmh0aXf0Fm-g"
     }
 })
--- utils/mdx-utils.js
+++ utils/mdx-utils.js
@@ -12,8 +12,11 @@ export const getPosts = async () => {
 
 export const getPostBySlug = async (id) => {
 
-    //TODO: BUSCAR UM POST EM ESPECIFICO.
-    //const {data} = await api.get(`/post?id=eq.${id}`)
+    const {data} = await api.get(`/posts?id=eq.${id}`)
+    
+    if(data){
+        return data[0];
+    }
+    return []    
 
-    return {}
 }
